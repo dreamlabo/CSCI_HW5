24ea79fcfd3526d65315b659fbbd2c122a2a0651
dreamlabo <toddlabo@gmail.com>
2019-04-03 20:24:04 -0600

    "changed something"

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logokkkk" />
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and SAVE to reload dummy.
           </p>
           <a
             className="App-link"
